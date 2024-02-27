34a076c8cba23e41bd8264e12a7be2b9ab6add97
netolobo <desilio@gmail.com>
2024-02-27 08:33:42 -0800

    Initial Commit

---
+++ HowToCheckInternetConnectionSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowToCheckInternetConnectionSwiftUI
+//
+//  Created by Neto Lobo on 27/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowToCheckInternetConnectionSwiftUI/HowToCheckInternetConnectionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCheckInternetConnectionSwiftUIApp.swift
+//  HowToCheckInternetConnectionSwiftUI
+//
+//  Created by Neto Lobo on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HowToCheckInternetConnectionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
